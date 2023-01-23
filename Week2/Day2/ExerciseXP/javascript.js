0d5bd575f82be80d48a10db7abcd5e9a6a66b4ef
CodedavidperloW <mrdavidperlow@gmail.com>
2023-01-24 01:31:38 +0200

    conditionals

---
+++ Week2/Day2/ExerciseXP/javascript.js
@@ -0,0 +1,58 @@
+//Exercise 1: Simple If/Else Statement
+
+let x = 5;
+let y = 2;
+
+if(x>y) {
+    console.log("x is the biggest number")
+} else if (y>x){
+    console.log("y is the biggest number")
+}
+console.log;
+
+
+//Exercise 2 Chihuahua
+
+let newDog = "Cat";
+console.log(newDog.length);
+console.log(newDog.toLowerCase);
+console.log(newDog.toUpperCase);
+
+if(newDog=== "Chihuahua"){
+    console.log("I love Chihuahuas, its my favorite dog breed")
+} else {
+    console.log("I don't care, I prefer cats")
+}
+
+//Exercise 3 Even Odd
+const userNumber = prompt("What is your number?");
+if(userNumber%2===0){
+    console.log("This is an even number")
+} else{
+    console.log("This is an odd number")
+}
+
+//Exercise 4
+//My logic is to change users to a number.
+//Then to change the array to read <name_user1>.
+//Make a conditional if / if else, to display the prompt.
+// const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
+// Number(users);
+// console.log();
+//if (user===1){
+//    console.log()
+//}
+
+if (users.length === 0){
+    console.log("No one is online");
+} else if (users.length === 1){
+    console.log(`${users[0]} is online`);
+} else if (users.length === 2){
+    console.log(`${users[0]} and ${users[1]} are online`);
+}  else if (users.length > 2){
+    const remainingUsers = users.length -2
+    console.log(`${users[0]} and ${users[1]} and ${remainingUsers} more are online`);
+    
+}
+
+
