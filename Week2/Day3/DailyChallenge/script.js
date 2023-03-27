4346710e918530b93b28ae4a6d1957897303aab0
CodedavidperloW <mrdavidperlow@gmail.com>
2023-03-27 10:23:56 +0300

    stars

--- Week2/Day3/DailyChallenge/script.js
+++ Week2/Day3/DailyChallenge/script.js
@@ -1,7 +1,19 @@
+const maxNumber = 6
+
+for (let i = 0; i < maxNumber; i++) {
+    let star = "*"
+    if (i === 0){ console.log(star) }
+    else if(i=== 1){console.log(star.repeat(2))}
+    else if(i=== 2){console.log(star.repeat(3))}
+    else if(i=== 3){console.log(star.repeat(4))}
+    else if(i=== 4){console.log(star.repeat(5))}
+    else if(i=== 5){console.log(star.repeat(6))}
+    else if(i=== 6){console.log(star.repeat(7))}
+}
+
+
+
 // const count = 0;
-
-
-
 // ////const myString = ['*','**','***','****','*****','******'];
 // const myString = [0, 1, 2, 3, 4, 5,]
 // for (item of myString) {
@@ -21,21 +33,21 @@
 //                     }}}}}}
 
 
-//<!----XP GOLD
+//<!----DailyChallenge GOLD
 
-const myString =[1,2,3,4,5,6];
-console.log(myString);
-for(item of myString){
-    if(item<=6){
-        console.log('*'.repeat(item));
-    }
-}
+// const myString =[1,2,3,4,5,6];
+// console.log(myString);
+// for(item of myString){
+//     if(item<=6){
+//         console.log('*'.repeat(item));
+//     }
+// }
 
-const numbers = [5,0,9,1,7,4,2,6,3,8];
-var myArrToString= numbers.toString();
-console.log(myArrToString);
-let myNewArrToString = numbers.join('+');
-console.log(myNewArrToString);
+// const numbers = [5,0,9,1,7,4,2,6,3,8];
+// var myArrToString= numbers.toString();
+// console.log(myArrToString);
+// let myNewArrToString = numbers.join('+');
+// console.log(myNewArrToString);
 
 
 
